482cc9bff7da5e20d8d44efa0044d583ddd958e6
NudeOilSun <94184352+NudeOilSun@users.noreply.github.com>
2025-08-18 16:30:32 +0100

    added pacman

--- src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -2,17 +2,21 @@ package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.PacmanGame;
 import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
         var marioGame = new MarioGame();
         var superContraGame = new SuperContraGame();
+        var pacmanGame = new PacmanGame();
 
         var superContraGameRunner = new GameRunner(superContraGame);
         var marioGameRunner = new GameRunner(marioGame);
+        var pacmanGameRunner = new GameRunner(pacmanGame);
 
         superContraGameRunner.Run();
         marioGameRunner.Run();
+        pacmanGameRunner.Run();
     }
 }
